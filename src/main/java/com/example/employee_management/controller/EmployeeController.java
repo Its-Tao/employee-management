1ef4007952b198e12d213a51cf1b7ba632869180
Its-Tao <taofinanaswabu@gmail.com>
2026-09-13 12:45:58 +0300

    testing get request using postman

---
+++ src/main/java/com/example/employee_management/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.employee_management.controller;
+
+import com.example.employee_management.model.Employee;
+import com.example.employee_management.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
