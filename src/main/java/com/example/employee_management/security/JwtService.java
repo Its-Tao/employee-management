6dcc7c2952a73cd32baf267295a41cf8cf4e820b
Its-Tao <taofinanaswabu@gmail.com>
2026-09-16 23:40:56 +0300

    update

---
+++ src/main/java/com/example/employee_management/security/JwtService.java
@@ -0,0 +1,27 @@
+package com.example.employee_management.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey =
+            Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(secretKey)
+                .compact();
+    }
+}
